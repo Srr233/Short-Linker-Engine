107986d37607357fcc64d87399dd0fc312efe843
Siarhei Saltanovich <siarhei_saltanovich@epam.com>
2021-12-14 15:42:37 +0300

    localMongodb v1: add remote

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,19 @@ import mongoose from "mongoose";
 import cors from 'cors';
 import mongo_uri from "./general date/mongo_uri";
 import redirectRouter from "./modules/routers/redirectRouter";
+import creds from "./general date/creds";
 
 const env_port = process.env.PORT || 3000;
 
 const app = express();
 
 const start = async () => {
-    await mongoose.connect(mongo_uri);
+    await mongoose.connect(mongo_uri, {
+        auth: {
+            username: creds.user,
+            password: creds.pwd
+        }
+    });
     app.use(cors());
     app.use(redirectRouter);
     app.listen(env_port, () => {
